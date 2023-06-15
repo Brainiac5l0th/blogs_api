34757dd5f9ecf900e118cbc5f157fe7e1306a825
shawonTalukder <shawontalukder.study@gmail.com>
2023-06-16 00:38:29 +0600

    database configuration

---
+++ config/db.js
@@ -0,0 +1,26 @@
+/*
+ *
+ *
+ ------->Title: database configuration
+ ->Description: this file is to configure database connection
+ ------>Author: Shawon Talukder
+ -------->Date: 06/16/2023
+ *
+ *
+ */
+
+// Dependencies
+const { Pool } = require("pg");
+
+
+// Configuration
+const pool = new Pool({
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD
+})
+
+// Export Model
+module.exports = pool;
