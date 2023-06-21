cddf0e7a6dc4fa2253986a32710f887849ce80dd
shawonTalukder <shawontalukder.study@gmail.com>
2023-06-22 01:12:07 +0600

    update user queries.

--- queries/userQueries.js
+++ queries/userQueries.js
@@ -22,11 +22,20 @@ userQueries.getUsersQuery = "SELECT users.user_id, users.firstName, users.middle
 // @SELECT/GET: user by id
 userQueries.getUserByIdQuery = "SELECT * FROM users JOIN user_roles USING (role_id) WHERE users.user_id = $1";
 
+// @SELECT/GET: user by email or id 
+userQueries.userCheckQuery = "SELECT * FROM users WHERE user_id = $1 OR email = $2 ";
+
 // @SELECT/GET: user by email 
 userQueries.duplicateEmailCheckQuery = "SELECT u FROM users u WHERE u.email = $1";
 
 // @INSERT/CREATE: create user  
 userQueries.addUserQuery = "INSERT INTO users (firstName, middleName, lastName, email, user_password, date_of_birth) VAlUES ($1, $2, $3, $4, $5, $6);";
 
+// @ALTER/UPDATE: update user  
+userQueries.updateUserFirstNameQuery = "UPDATE users SET firstName = $1 WHERE user_id = $2;"
+userQueries.updateUserLastNameQuery = "UPDATE users SET lastName = $1 WHERE user_id = $2;"
+userQueries.updateUserMiddleNameQuery = "UPDATE users SET middleName = $1 WHERE user_id = $2;"
+userQueries.updateUserPasswordQuery = "UPDATE users SET user_password = $1 WHERE user_id = $2;"
+userQueries.updateUserDOBQuery = "UPDATE users SET date_of_birth = $1 WHERE user_id = $2;"
 // Export Model
 module.exports = userQueries;
