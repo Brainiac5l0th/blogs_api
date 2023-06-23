37406902cdf48a2f01132f211bfc89249c9412f9
shawonTalukder <shawontalukder.study@gmail.com>
2023-06-23 23:18:23 +0600

    auth route initialized!

---
+++ controller/authController.js
@@ -0,0 +1,21 @@
+/*
+ *
+ *
+ ------->Title: auth controller
+ ->Description: this controller is to provide all type of auth controller function
+ ------>Author: Shawon Talukder
+ -------->Date: 06/23/2023
+ *
+ *
+ */
+
+// Dependencies
+
+// Model Scaffolding
+const authController = {};
+
+// Model Structure
+
+
+// Export Model
+module.exports = authController;
---
+++ routes/authRoute.js
@@ -0,0 +1,22 @@
+/*
+ *
+ *
+ ------->Title: auth router
+ ->Description: this router is to handle user login, logout, JWT token
+ ------>Author: Shawon Talukder
+ -------->Date: 06/23/2023
+ *
+ *
+ */
+
+// Dependencies
+const express = require("express");
+const { } = require("../controller/authController");
+
+// Model Scaffolding
+const authRouter = express.Router();
+
+// Model Structure
+
+// Export Model
+module.exports = authRouter;
