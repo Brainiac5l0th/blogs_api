e1ec0a01a6ca2d7e1ea7c7415a9f516bb3544292
shawonTalukder <shawontalukder.study@gmail.com>
2023-06-26 01:10:24 +0600

    get all blogs combining with likes and comments

--- controller/blogController.js
+++ controller/blogController.js
@@ -1,21 +1,36 @@
 /*
  *
  *
- ------->Title: user controller
- ->Description: 
+ ------->Title: blogs controller
+ ->Description: this controller file is to handle all blogs related content
  ------>Author: Shawon Talukder
- -------->Date: 06/16/2023
+ -------->Date: 06/25/2023
  *
  *
  */
 
 // Dependencies
-
+const pool = require("../config/db");
+const {
+    getAllBlogs
+} = require("../queries/blogQueries");
 
 // Model Scaffolding
 const blogController = {};
 
 // Model Structure
 
+// @GET: all blogs
+blogController.getBlogs = async (req, res) => {
+    try {
+        const result = await pool.query(getAllBlogs);
+        if (result.rowCount === 0) {
+            return res.status(204).json({ message: "No blogs found!" })
+        }
+        return res.status(200).json({ message: "success!", data: result.rows })
+    } catch (error) {
+        res.status(500).json({ message: "There is a server side error!" })
+    }
+}
 // Export Model
 module.exports = blogController;
---
+++ queries/blogQueries.js
@@ -0,0 +1,48 @@
+/*
+ *
+ *
+ ------->Title: blog queries
+ ->Description: this is query file for blogs
+ ------>Author: Shawon Talukder
+ -------->Date: 06/23/2023
+ *
+ *
+ */
+
+// Model Scaffolding
+const blogQueries = {};
+
+// Model Structure
+blogQueries.getAllBlogs =
+    `SELECT 
+        blogs.blog_id, 
+        blogs.blog_title, 
+        blogs.blog_description, 
+        blogs.blog_banner, 
+        blogs.blog_status,
+        (
+            SELECT COUNT(*)::INTEGER as likes 
+            FROM likes 
+            WHERE likes.blog_id =  blogs.blog_id
+        ), 
+        (
+            SELECT COUNT(*)::INTEGER as comments 
+            FROM comments 
+            WHERE comments.blog_id = blogs.blog_id
+        ), 
+        (
+            SELECT 
+                CONCAT(users.firstName,' ',users.lastName)  as author_name 
+            FROM users 
+            WHERE blogs.author_id = users.user_id
+        ),
+        blogs.created_at,
+        blogs.author_id
+    FROM blogs
+    LEFT JOIN users ON blogs.author_id = users.user_id
+    LEFT JOIN likes USING(blog_id)
+    LEFT JOIN comments USING(blog_id)
+    ORDER BY blogs.created_at;`;
+
+// Export Model
+module.exports = blogQueries;
