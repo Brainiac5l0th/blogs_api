aafed6a7bc1e891ab0dd3f2be4d88d1672ee4883
shawonTalukder <shawontalukder.study@gmail.com>
2023-06-28 02:39:28 +0600

    service to send email

---
+++ services/sendMail.js
@@ -0,0 +1,33 @@
+/*
+ *
+ *
+ ------->Title: send mail
+ ->Description: this file is to send mail to the user
+ ------>Author: Shawon Talukder
+ -------->Date: 06/28/2023
+ *
+ *
+ */
+// dependencies
+
+// model structure
+/*
+ * @params: 
+ *      email:string   -> email address of the receiver
+ *      subject:string -> subject of the email
+ *      message:string -> message in the message body
+ * 
+ * @returns: the function returns true if sent successfully else false
+ */
+
+const sendMail = async (email, subject, message) => {
+    try {
+        
+
+    } catch (error) {
+
+    }
+}
+
+// export model
+module.exports = sendMail
