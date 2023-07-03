074c7f0b19bb8608734710320b23f73f3ca111d9
shawonTalukder <shawontalukder.study@gmail.com>
2023-07-03 18:36:22 +0600

    nodemailer addded.. send mail from one admin mail id.

--- services/sendMail.js
+++ services/sendMail.js
@@ -9,27 +9,51 @@
  *
  */
 // dependencies
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 // model structure
-
-
-const sendMail = async (email, subject, message) => {
+const sendMailer = (email, subject, messageHTML) => {
     /*
      * @params: 
      *      email:string   -> email address of the receiver
      *      subject:string -> subject of the email
-     *      message:string -> message in the message body
+     *      messageHTML:string -> message in the message body
      * 
      * @returns: the function returns true if sent successfully else false
      * 
     */
-    try {
+    // declaring constants
+    const USER_SENDER = process.env.NODEMAILER_USER;
+    const USER_PASSWORD = process.env.NODEMAILER_PASS;
 
 
-    } catch (error) {
-
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: USER_SENDER,
+            pass: USER_PASSWORD
+        }
+    });
+    const messageBody = {
+        // sender address
+        from: USER_SENDER,
+        // list of receivers
+        to: email,
+        subject, // Subject line
+        html: messageHTML, // plain text body
     }
+
+    transporter.sendMail(messageBody, (err, info) => {
+        if (err || !info.accepted.length > 0) {
+            return false;
+        }
+        return true;
+    });
 }
 
+
 // export model
-module.exports = sendMail
+module.exports = {
+    sendMailer
+}
