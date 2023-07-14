9b76644ef764b77a6349e6cfc25528c61adacc0d
shawonTalukder <shawontalukder.study@gmail.com>
2023-07-14 23:38:59 +0600

    url change

--- config/db.js
+++ config/db.js
@@ -15,12 +15,7 @@ const { Pool } = require("pg");
 
 // Configuration
 const pool = new Pool({
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    database: process.env.PGDATABASE,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    ssl: require
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
 
 
